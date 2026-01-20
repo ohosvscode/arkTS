74e6d07e4b0c364ce77a4fc795832f82ccd74d61
groupguanfang <1203970284@qq.com>
2026-01-20 15:44:06 +0800

    feat: update release script

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,11 +1,32 @@
 import { execSync } from 'node:child_process'
 import fs from 'node:fs'
 import path from 'node:path'
-import process from 'node:process'
 import { fileURLToPath } from 'node:url'
+import fg from 'fast-glob'
 import { globalLogger as logger } from 'tsdown'
 
 const dirname = fileURLToPath(new URL('.', import.meta.url))
+const PROJECT_ROOT = path.resolve(dirname, '..')
+const EXTENSION_TARGETS = [
+  'win32-x64',
+  'win32-arm64',
+  'linux-x64',
+  'linux-arm64',
+  'linux-armhf',
+  'darwin-x64',
+  'darwin-arm64',
+  'alpine-x64',
+  'alpine-arm64',
+  'web',
+] as const
+
+function getExtensionPackagePath(target: (typeof EXTENSION_TARGETS)[number]): string {
+  const packagePath = path.resolve(PROJECT_ROOT, 'packages', 'vscode', `vscode-naily-ets-${target}-*.vsix`)
+  const packages = fg.sync(packagePath)
+  if (packages.length === 0) throw new Error(`No extension package found for target ${target}: ${Object.values(fg.sync(path.resolve(PROJECT_ROOT, 'packages', 'vscode', '*.vsix'))).join(', ')}`)
+  if (packages.length > 1) throw new Error(`Multiple extension packages found for target ${target}: ${packages.join(', ')}`)
+  return packages[0]
+}
 
 function publishNpmPackages(): boolean {
   try {
@@ -23,37 +44,39 @@ function publishNpmPackages(): boolean {
 }
 
 function publishToVsce(): boolean {
-  try {
-    logger.info(`Publishing to VSCE...`)
-    const command = `pnpm -F vscode-naily-ets vsce ${process.argv.slice(2).includes('--dry-run') ? 'package' : 'publish'} ${process.argv.slice(2).filter(v => v !== '--dry-run').join(' ')}`
-    logger.info(`Executing command: ${command}`)
-    execSync(command, { cwd: path.resolve(dirname, '..'), stdio: 'inherit' })
-    return true
+  for (const target of EXTENSION_TARGETS) {
+    try {
+      const packagePath = getExtensionPackagePath(target)
+      logger.info(`Publishing to VSCE for target ${target}...`)
+      const command = `pnpm -F vscode-naily-ets vsce publish --target ${target} --packagePath ${packagePath}`
+      logger.info(`Executing command: ${command}`)
+      execSync(command, { cwd: path.resolve(dirname, '..'), stdio: 'inherit' })
+    }
+    catch (error) {
+      logger.error(`Failed to publish to VSCE for target ${target}: ${error instanceof Error ? error.message : String(error)}`)
+      console.error(error)
+      return false
+    }
   }
-  catch (error) {
-    logger.error(`Failed to publish to VSCE: ${error instanceof Error ? error.message : String(error)}`)
-    console.error(error)
-    return false
-  }
+  return true
 }
 
 function publishToOvsce(): boolean {
-  try {
-    if (process.argv.includes('--dry-run')) {
-      logger.warn(`Skipping OVSCE package in dry-run mode.`)
-      return true
+  for (const target of EXTENSION_TARGETS) {
+    try {
+      const packagePath = getExtensionPackagePath(target)
+      logger.info(`Publishing to OVSCE for target ${target}...`)
+      const command = `pnpm -F vscode-naily-ets ovsx publish --target ${target} --packagePath ${packagePath}`
+      logger.info(`Executing command: ${command}`)
+      execSync(command, { cwd: path.resolve(dirname, '..'), stdio: 'inherit' })
     }
-    logger.info(`Publishing to OVSCE...`)
-    const command = `pnpm -F vscode-naily-ets ovsx publish ${process.argv.slice(2).filter(v => v !== '--dry-run').join(' ')}`
-    logger.info(`Executing command: ${command}`)
-    execSync(command, { cwd: path.resolve(dirname, '..'), stdio: 'inherit' })
-    return true
+    catch (error) {
+      logger.error(`Failed to publish to OVSCE for target ${target}: ${error instanceof Error ? error.message : String(error)}`)
+      console.error(error)
+      return false
+    }
   }
-  catch (error) {
-    logger.error(`Failed to publish to OVSCE: ${error instanceof Error ? error.message : String(error)}`)
-    console.error(error)
-    return false
-  }
+  return true
 }
 
 ;(async () => {
