5f7bdc07d78fdba0ea9aac35f15b76105a4d6417
groupguanfang <1203970284@qq.com>
2026-01-20 14:00:19 +0800

    docs(changeset): add changelog

---
+++ .changeset/jolly-waves-raise.md
@@ -0,0 +1,7 @@
+---
+"@arkts/language-service": patch
+"@arkts/language-server": patch
+"vscode-naily-ets": patch
+---
+
+feat: 添加[oxk](https://github.com/ohos-rs/oxc-ark)代码格式化支持
--- packages/vscode/src/project/typed-router.d.ts
+++ packages/vscode/src/project/typed-router.d.ts
@@ -57,19 +57,19 @@ declare module 'vue-router/auto-routes' {
    * @internal
    */
   export interface _RouteFileInfoMap {
-    'src/project/pages/index.vue': {
+    'packages/vscode/src/project/pages/index.vue': {
       routes:
         | '/'
       views:
         | never
     }
-    'src/project/pages/template-detail.vue': {
+    'packages/vscode/src/project/pages/template-detail.vue': {
       routes:
         | '/template-detail'
       views:
         | never
     }
-    'src/project/pages/template-market.vue': {
+    'packages/vscode/src/project/pages/template-market.vue': {
       routes:
         | '/template-market'
       views:
