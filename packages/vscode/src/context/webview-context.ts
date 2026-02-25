7f06b070ee1cc103854dbc6d2395ef0047b1acea
groupguanfang <1203970284@qq.com>
2026-02-25 18:41:58 +0800

    feat: useCompiledView -> WebviewHtmlLoader

--- packages/vscode/src/context/webview-context.ts
+++ packages/vscode/src/context/webview-context.ts
@@ -1,15 +1,11 @@
 import type { ExtensionLogger } from '@arkts/shared/vscode'
 import type { BirpcReturn } from 'birpc'
+import type * as vscode from 'vscode'
 import type { FileSystemContext } from './file-system-context'
-import path from 'node:path'
+import type { WebviewContainer } from './webview-html-loader'
 import { createBirpc } from 'birpc'
-import * as vscode from 'vscode'
 import { keepClassInstanceThis } from '../utils'
-
-export interface WebviewContainer {
-  webview: vscode.Webview
-  onDidDispose(callback: () => void): void
-}
+import { WebviewHtmlLoader } from './webview-html-loader'
 
 export abstract class WebviewContext<RemoteFunctions extends WebviewContext.ClientFunction, LocalFunctions extends WebviewContext.ServerFunction<RemoteFunctions, LocalFunctions>> {
   private _currentConnection: BirpcReturn<RemoteFunctions, LocalFunctions> | undefined
@@ -25,27 +21,6 @@ export abstract class WebviewContext<RemoteFunctions extends WebviewContext.Clie
     this._currentConnection = undefined
   }
 
-  private async loadHtml<Container extends WebviewContainer>(webviewContainer: Container, htmlPath: string, initialURL?: string): Promise<void> {
-    const content = await this.fsx.readFileToString(vscode.Uri.file(htmlPath))
-    webviewContainer.webview.html = content.replace(/\{\{(.*?)\}\}/g, (_, href) => {
-      const resourceUri = webviewContainer.webview.asWebviewUri(vscode.Uri.joinPath(vscode.Uri.file(path.dirname(htmlPath)), href?.trim?.() || href))
-      return decodeURIComponent(resourceUri?.toString() || '')
-    }).replace(/<head>/, `<head>${initialURL ? `<script>window.INITIAL_URL = '${initialURL}'</script>` : ''}`)
-  }
-
-  private createHtmlWatcher<Container extends WebviewContainer>(webviewContainer: Container, htmlPath: string, initialURL?: string): vscode.Disposable {
-    const fsWatcher = vscode.workspace.createFileSystemWatcher(htmlPath)
-    const onDidChangeDisposable = fsWatcher.onDidChange(() => this.loadHtml(webviewContainer, htmlPath, initialURL))
-    const onDidAddDisposable = fsWatcher.onDidCreate(() => this.loadHtml(webviewContainer, htmlPath, initialURL))
-    this.loadHtml(webviewContainer, htmlPath, initialURL)
-
-    return new vscode.Disposable(() => {
-      onDidChangeDisposable.dispose()
-      onDidAddDisposable.dispose()
-      fsWatcher.dispose()
-    })
-  }
-
   protected async attachWebview<T extends WebviewContainer>(
     webviewContainer: T,
     extensionUri: vscode.Uri,
@@ -54,8 +29,8 @@ export abstract class WebviewContext<RemoteFunctions extends WebviewContext.Clie
     initialURL?: string,
   ): Promise<void> {
     serverFunction.onBeforeRpcInitialized?.(this._currentConnection as BirpcReturn<RemoteFunctions, LocalFunctions>)
-    webviewContainer.webview.html = await this.fsx.readFileToString(vscode.Uri.joinPath(extensionUri, 'build', htmlName))
-    const disposable = this.createHtmlWatcher(webviewContainer, vscode.Uri.joinPath(extensionUri, 'build', htmlName).fsPath, initialURL)
+    const webviewHtmlLoader = new WebviewHtmlLoader(this.fsx, extensionUri, webviewContainer, htmlName, initialURL)
+    const htmlWatcherDisposable = await webviewHtmlLoader.createHtmlWatcher()
     this._currentConnection = createBirpc<RemoteFunctions, LocalFunctions>(
       keepClassInstanceThis(serverFunction as LocalFunctions),
       {
@@ -64,7 +39,8 @@ export abstract class WebviewContext<RemoteFunctions extends WebviewContext.Clie
         serialize: data => JSON.stringify(data),
         deserialize: data => JSON.parse(data),
         onFunctionError: (error, functionName, functionArgs) => {
-          console.error('Error in WebviewContext createBirpc onFunctionError, functionName: ', functionName, 'functionArgs: ', functionArgs)
+          this.logger.getConsola().error(`Error in WebviewContext ${htmlName} createBirpc onFunctionError, functionName: ${functionName}, functionArgs: ${functionArgs}`)
+          this.logger.getConsola().error(error)
           console.error(error)
         },
       },
@@ -73,7 +49,7 @@ export abstract class WebviewContext<RemoteFunctions extends WebviewContext.Clie
     this.logger.getConsola().info(`Webview ${htmlName} created.`)
     webviewContainer.onDidDispose(() => {
       this.logger.getConsola().info(`Webview ${htmlName} disposed.`)
-      disposable.dispose()
+      htmlWatcherDisposable.dispose()
       this._currentConnection?.$close()
       serverFunction.dispose?.()
       this._currentConnection = undefined
---
+++ packages/vscode/src/context/webview-html-loader.ts
@@ -0,0 +1,54 @@
+import type { FileSystemContext } from './file-system-context'
+import { ExtensionLogger } from '@arkts/shared/vscode'
+import * as vscode from 'vscode'
+
+export interface WebviewContainer {
+  webview: vscode.Webview
+  onDidDispose(callback: () => void): void
+}
+
+export class WebviewHtmlLoader<Container extends WebviewContainer = WebviewContainer> extends ExtensionLogger {
+  constructor(
+    private readonly fsx: FileSystemContext,
+    private readonly extensionUri: vscode.Uri,
+    private readonly webviewContainer: Container,
+    private readonly htmlName: string,
+    private readonly initialURL?: string,
+  ) {
+    super()
+  }
+
+  private buildUri = vscode.Uri.joinPath(this.extensionUri, 'build')
+  private htmlUri = vscode.Uri.joinPath(this.buildUri, this.htmlName)
+
+  getBuildUri(): vscode.Uri {
+    return this.buildUri
+  }
+
+  getHtmlUri(): vscode.Uri {
+    return this.htmlUri
+  }
+
+  private async loadHtml(): Promise<void> {
+    const content = (await this.fsx.readFileToString(this.htmlUri)).replace(/\{\{(.*?)\}\}/g, (_, href) => {
+      const resourceUri = this.webviewContainer.webview.asWebviewUri(vscode.Uri.joinPath(this.getBuildUri(), href?.trim?.() || href))
+      return decodeURIComponent(resourceUri?.toString() || '')
+    }).replace(/<head>/, `<head>${this.initialURL ? `<script>window.INITIAL_URL = '${this.initialURL}'</script>` : ''}`)
+    this.webviewContainer.webview.html = content
+  }
+
+  public async createHtmlWatcher(): Promise<vscode.Disposable> {
+    await this.loadHtml()
+    const relativePattern = new vscode.RelativePattern(this.buildUri, '**/*')
+    const fsWatcher = vscode.workspace.createFileSystemWatcher(relativePattern)
+    this.getConsola().info(`WebviewHtmlLoader created html watcher for ${relativePattern.baseUri.fsPath} -> ${relativePattern.pattern}`)
+    const onDidChangeDisposable = fsWatcher.onDidChange(() => this.loadHtml())
+    const onDidAddDisposable = fsWatcher.onDidCreate(() => this.loadHtml())
+
+    return new vscode.Disposable(() => {
+      onDidChangeDisposable.dispose()
+      onDidAddDisposable.dispose()
+      fsWatcher.dispose()
+    })
+  }
+}
